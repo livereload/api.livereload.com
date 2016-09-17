51c4b8fac92792187aa1041c1d192e1c8ceaa326
Andrey Tarantsov <andrey@tarantsov.com>
2016-09-17 22:22:53 +0700

    Useless commit

--- main.go
+++ main.go
@@ -5,9 +5,16 @@ import (
     "log"
     "net/http"
     "os"
+    "strings"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
+func importLicenses(w http.ResponseWriter, r *http.Request) {
+    components := strings.Split(r.URL.Path, "/")
+
+    fmt.Fprintf(w, "Import %+v!", components)
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
     fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
 }
 
@@ -17,7 +24,8 @@ func main() {
         port = "3000"
     }
 
-    http.HandleFunc("/", handler)
+    http.HandleFunc("/licensing/admin/import/", importLicenses)
+    http.HandleFunc("/", index)
     log.Printf("Listening on port %s.", port)
     http.ListenAndServe(fmt.Sprintf(":%s", port), nil)
 }
